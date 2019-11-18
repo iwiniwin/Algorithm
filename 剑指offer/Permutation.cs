6437c86721379d6f92b5d6089b0fafe96464e2a6
LensarZhang <LensarZhang@126.com>
2019-11-18 20:21:29 +0800

    ﻿fix: add the second solution to the Permutation
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

--- 剑指offer/Permutation.cs
+++ 剑指offer/Permutation.cs
@@ -24,6 +24,23 @@ using System.Collections.Generic;
 namespace Permutation {
     class Solution {
 
+        /*解法1，递归
+        基本思路：
+        对于目标字符串，每次首先确定第一个字符。例如对于字符串abc，则第一个字符可以是a或b或c
+        然后将除去选取的第一个字符外的剩下字符串进行递归，重复上面的操作，直到剩下的字符串长度为1为止。
+        通过HashSet过滤重复的字符串，通过Sort方法进行排序
+
+        对于字符串abc，递归过程如下所示
+        选取a为第一个字符（剩下字符串bc）
+            选取b为第一个字符（剩下字符串c，长度为1，结束低估，得到字符串abc）
+            选取c为第一个字符（剩下字符串b，长度为1，结束低估，得到字符串acb）
+        选取b为第一个字符（剩下字符串ac）
+            选取a为第一个字符（剩下字符串c，长度为1，结束低估，得到字符串bac）
+            选取c为第一个字符（剩下字符串b，长度为1，结束低估，得到字符串bca）
+        选取c为第一个字符（剩下字符串ab）
+            选取a为第一个字符（剩下字符串c，长度为1，结束低估，得到字符串cab）
+            选取b为第一个字符（剩下字符串b，长度为1，结束低估，得到字符串cba）
+        */
         public void PermutationImpl(string pre, string str, HashSet<string> set)
         {
             if (str.Length == 1) {
@@ -44,6 +61,59 @@ namespace Permutation {
             return list;
         }
 
+        /*解法2，递归
+        基本思路：
+        和解法1思路相同。区别在于选取首字符后剩余字符串的表示和重复字符串的过滤
+
+        对剩余字符串的处理，解法1是通过str.Remove(i, 1)返回的新字符串表示剩余字符串
+        解法2是通过char数组，swap方法依次将选取的字符和index位置的元素互换，然后index+1及之后的就是剩余字符串
+
+        对重复字符串的处理，解法1是通过HashSet的特性自动过滤重复字符串（即使往HashSet中Add重复字符串也会被自动忽略）
+        解法2是通过算法判断是重复字符串则不再Add
+        */
+        public void Swap(char[] chars, int i, int j){
+            char temp = chars[i];
+            chars[i] = chars[j];
+            chars[j] = temp;
+        }
+        public void PermutationImpl2(char[] chars, int index, List<string> list)
+        {
+            if (index == chars.Length){
+                list.Add(new string(chars));
+                return;
+            }
+            HashSet<char> set = new HashSet<char>();
+            for(int i = index; i < chars.Length && !set.Contains(chars[i]); i ++){
+                set.Add(chars[i]);
+                Swap(chars, i, index);
+                PermutationImpl2(chars, index + 1, list);
+                Swap(chars, index, i);
+            }
+        }
+        public List<string> Permutation2(string str)
+        {
+            List<string> list = new List<string>();
+            if (str != null && str.Length > 0){
+                char[] chars = str.ToCharArray();
+                PermutationImpl2(chars, 0, list);
+                list.Sort();
+            }
+            return list;
+        }
+
+        /*
+        abc
+
+        bac
+        cba
+
+        bac
+        acb
+
+        cba
+        acb
+
+        */
         public void Print(List<string> list) {
             foreach (string item in list)
             {
@@ -64,6 +134,7 @@ c   aba
 */
         public void Test() {
             Print(Permutation("aba"));
+            Print(Permutation2(""));
         }
     }
 }
