855d77014006d812f2335472383788a6dce9900f
iwiniwin <iwiniwin@foxmail.com>
2019-11-23 23:16:09 +0800

    add two solutions to the FindGreatestSumOfSubArray

--- 剑指offer/FindGreatestSumOfSubArray.cs
+++ 剑指offer/FindGreatestSumOfSubArray.cs
@@ -22,15 +22,62 @@ using System;
 namespace FindGreatestSumOfSubArray {
     class Solution {
 
+        /// <summary>
+        /// 解法1
+        /// 基本思路：
+        /// 依次遍历数组元素，是否吸收前面的元素作为连续子数组的前缀的标准是，它们的和是否为正数
+        /// 例如当遍历到元素A时
+        /// 判断元素A前面的元素和是否是正数，是正数，则加上A对最终的结果产生积极影响，保留前面的元素
+        /// 否则丢弃前面的元素直接从A开始继续寻找最大和。
+        /// </summary>
         public int FindGreatestSumOfSubArray(int[] array)
         {
-            // write code here
-            return 0;
+            if(array == null || array.Length == 0){
+                return 0;
+            }
+            int max = array[0];
+            int sum = max;
+            for(int i = 1; i < array.Length; i ++){
+                if(sum < 0){
+                    sum = array[i];
+                }else{
+                    sum += array[i];
+                }
+                if(sum > max){
+                    max = sum;
+                }
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// 解法2，动态规划
+        /// 基本思路：
+        /// 可以这样理解，假设前n-1个连续元素最大和为x，
+        /// 那前n个连续元素最大和就是x加最后一个元素值和最后一个元素值中的较大值
+        /// F(n) = max(F(n -1) + array[n], array[n])，这里的array[n]表示数组末尾元素的值
+        /// 注意这里的F(n)，并不表示最终结果。最终结果应该是F(1),F(2)..F(n)中的较大值
+        /// </summary>
+        public int FindGreatestSumOfSubArray2(int[] array)
+        {
+            if(array == null || array.Length == 0){
+                return 0;
+            }
+            int f = array[0], m = f;
+            for(int i = 1; i < array.Length; i ++){
+                f = (f + array[i]) > array[i] ? (f + array[i]) : array[i];
+                m = m > f ? m : f;
+            }
+            return m;
         }
 
         public void Test() {
-            int[] array = new int[]{6,-3,-2,7,-15,1,2,2};
-            Console.WriteLine(FindGreatestSumOfSubArray(array));
+            int[] array = new int[]{6,-3,-2,7,-15,1,2,6};
+            array = new int[]{-9, -8, 2, -1, 3};
+            // array = null;
+
+            // Console.WriteLine(FindGreatestSumOfSubArray(array));
+            Console.WriteLine(FindGreatestSumOfSubArray2(array));
         }
 
     }
