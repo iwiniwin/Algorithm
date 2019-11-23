bec4a37c61ac1ebace2165497e34092a29451d37
iwiniwin <iwiniwin@foxmail.com>
2019-11-23 23:27:18 +0800

    add NumberOf1Between1AndN problem

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace Algorithm
             // Permutation.Solution s = new Permutation.Solution();
             // MoreThanHalfNum.Solution s = new MoreThanHalfNum.Solution();
             // GetLeastNumbers.Solution s = new GetLeastNumbers.Solution();
-            FindGreatestSumOfSubArray.Solution s = new FindGreatestSumOfSubArray.Solution();
+            // FindGreatestSumOfSubArray.Solution s = new FindGreatestSumOfSubArray.Solution();
+            NumberOf1Between1AndN.Solution s = new NumberOf1Between1AndN.Solution();
 
             s.Test();
         }
---
+++ 剑指offer/NumberOf1Between1AndN.cs
@@ -0,0 +1,34 @@
+/*
+题目名称：
+整数中1出现的次数（从1到n整数中1出现的次数）
+
+题目描述：
+求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？
+为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+ACMer希望你们帮帮他,并把问题更加普遍化,
+可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）。
+
+代码结构：
+class Solution
+{
+    public int NumberOf1Between1AndN_Solution(int n)
+    {
+        // write code here
+    }
+}
+*/
+using System;
+namespace NumberOf1Between1AndN {
+    class Solution {
+
+        public int NumberOf1Between1AndN_Solution(int n)
+        {
+            return 0;
+        }
+
+        public void Test() {
+            Console.WriteLine(NumberOf1Between1AndN_Solution(5));
+        }
+
+    }
+}
