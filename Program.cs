49d2ef198964d01a90074e88fab37476d5ed89a4
iwiniwin <iwiniwin@foxmail.com>
2019-11-26 13:33:23 +0800

    ﻿fix: add PrintMinNumber problem
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ namespace Algorithm
         static void Main(string[] args)
         {
             // 剑指offer
-            // Permutation.Solution s = new Permutation.Solution();
-            // MoreThanHalfNum.Solution s = new MoreThanHalfNum.Solution();
-            // GetLeastNumbers.Solution s = new GetLeastNumbers.Solution();
-            // FindGreatestSumOfSubArray.Solution s = new FindGreatestSumOfSubArray.Solution();
-            // NumberOf1Between1AndN.Solution1 s = new NumberOf1Between1AndN.Solution();
-            // s.Test();
+            // new Permutation.Solution().Test();
+            // new MoreThanHalfNum.Solution().Test();
+            // new GetLeastNumbers.Solution().Test();
+            // new FindGreatestSumOfSubArray.Solution().Test();
+            // new NumberOf1Between1AndN.Solution().Test();
+            new PrintMinNumber.Solution().Test();
 
             // 程序员代码面试指南
-            NumberOf1From1ToN.Solution.Test();
+            // NumberOf1From1ToN.Solution.Test();
             
         }
     }
---
+++ 剑指offer/PrintMinNumber.cs
@@ -0,0 +1,35 @@
+/*
+题目名称：
+把数组排成最小的数
+
+题目描述：
+输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
+
+代码结构：
+class Solution
+{
+    public string PrintMinNumber(int[] numbers)
+    {
+        // write code here
+    }
+}
+*/
+using System;
+namespace PrintMinNumber {
+    class Solution {
+
+        public string PrintMinNumber(int[] numbers)
+        {
+            return "";
+        }
+
+        public void Test() {
+            int[] numbers = new int[]{3, 32, 321};
+            // numbers = new int[]{};
+            // numbers = null;
+
+            Console.WriteLine(PrintMinNumber(numbers));
+        }
+    }
+}
