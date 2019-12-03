d53a0ec162e3be8bc57dd439b4a4a0983c0c941d
iwiniwin <iwiniwin@foxmail.com>
2019-12-03 22:26:29 +0800

    add InversePairs problem

---
+++ 剑指offer/InversePairs.cs
@@ -0,0 +1,41 @@
+/*
+题目名称：
+数组中的逆序对
+
+题目描述：
+在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。
+输入一个数组,求出这个数组中的逆序对的总数P。
+并将P对1000000007取模的结果输出。 即输出P%1000000007
+
+输入描述：
+题目保证输入的数组中没有的相同的数字
+数据范围：
+	对于%50的数据,size<=10^4
+	对于%75的数据,size<=10^5
+	对于%100的数据,size<=2*10^5
+
+代码结构：
+class Solution
+{
+    public int InversePairs(int[] data)
+    {
+        // write code here
+    }
+}
+*/
+using System;
+namespace InversePairs {
+    class Solution {
+
+        public int InversePairs(int[] data)
+        {
+            return 0;
+        }
+
+        public void Test() {
+            int[] arr = new int[]{};
+
+            Console.WriteLine(InversePairs(arr));
+        }
+    }
+}
