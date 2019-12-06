d06067526052404d571de88aa4d1127eeed788fc
iwiniwin <iwiniwin@foxmail.com>
2019-12-06 14:11:44 +0800

    add the solution to the InversePairs

--- README.md
+++ README.md
@@ -48,6 +48,21 @@
 
 </tr>
 
+<tr>
+<td width="25%" align="center">
+<a href="https://github.com/iwiniwin/Algorithm/blob/master/%E5%89%91%E6%8C%87offer/InversePairs.cs">数组中的逆序对</a>
+</td>
+<td width="25%" align="center">
+
+</td>
+<td width="25%" align="center">
+
+</td>
+<td width="25%" align="center">
+
+</td>
+</tr>
+
 </tbody></table>
 
 ## 程序员代码面试指南
--- 剑指offer/InversePairs.cs
+++ 剑指offer/InversePairs.cs
@@ -27,15 +27,69 @@ using System;
 namespace InversePairs {
     class Solution {
 
+        /// <summary>
+        /// 解法，归并排序思想
+        /// 基本思路：
+        /// 将含有n个元素的数组，不断的进行二分为两个子数组。直到每个子数组都只含有一个元素。
+        /// 再进行合并两个子数组，利用一个临时数组，不断从两个子数组中选出最大值放到临时数组中。
+        /// 在比较时，由于每个子数组都是有序递减的，所以只需要比较子数组的首元素即可（此时进行逆序对的数量统计，如果
+        /// 该元素已经大于右边子数组的首元素，则右边子数组的所有元素都小于该元素，即都可以和该元素构成逆序对）。
+        /// 归并排序介绍 https://blog.csdn.net/FightLei/article/details/52586776
+        /// </summary>
+
+        public int Merge(int[] data, int start, int mid, int end){
+            int count = 0;
+            int i = start;
+            int j = mid + 1;
+            int k = 0;
+            int[] temp = new int[end - start + 1];
+            while(i <= mid && j <= end){
+                if(data[i] > data[j]){
+                    count += (end - j + 1);
+                    if(count > 1000000007){
+                        count %= 1000000007;
+                    }
+                    temp[k ++] = data[i ++];
+                }else{
+                    temp[k ++] = data[j ++];
+                }
+            }
+            while(i <= mid){
+                temp[k ++] = data[i ++];
+            }
+            while(j <= end){
+                temp[k ++] = data[j ++];
+            }
+            for(i = 0; i < k; i ++){
+                data[start + i] = temp[i];
+            }
+            return count;
+        }
+
+        public int MergeSort(int[] data, int start, int end){
+            if(start >= end){
+                return 0;
+            }
+            int mid = (start + end) / 2;
+            return (MergeSort(data, start, mid) + MergeSort(data, mid + 1, end) + Merge(data, start, mid, end)) % 1000000007;
+        }
+
         public int InversePairs(int[] data)
         {
-            return 0;
+            if(data == null){
+                return 0;
+            }
+            return MergeSort(data, 0, data.Length - 1);
         }
 
         public void Test() {
-            int[] arr = new int[]{};
+            int[] data = new int[]{1, 6, 2, 8, 4, 1, 0, 7};
+            // data = new int[]{1, 2, 3, 4, 5, 6, 7, 0};
+            // data = null;
+            // data = new int[]{};
+            // data = new int[]{1, 1, 0};
 
-            Console.WriteLine(InversePairs(arr));
+            Console.WriteLine(InversePairs(data));
         }
     }
 }
