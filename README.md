f80f95973d6dcd3496c64d6130f941affbf23ab4
iwiniwin <iwiniwin@foxmail.com>
2019-12-10 14:01:40 +0800

    add the solution to the GetNumberOfK

--- README.md
+++ README.md
@@ -56,7 +56,7 @@
 <a href="https://github.com/iwiniwin/Algorithm/blob/master/%E5%89%91%E6%8C%87offer/FindFirstCommonNode.cs">两个链表的第一个公共结点</a>
 </td>
 <td width="25%" align="center">
-
+<a href="https://github.com/iwiniwin/Algorithm/blob/master/%E5%89%91%E6%8C%87offer/GetNumberOfK.cs">数字在排序数组中出现的次数</a>
 </td>
 <td width="25%" align="center">
 
--- 剑指offer/GetNumberOfK.cs
+++ 剑指offer/GetNumberOfK.cs
@@ -19,13 +19,55 @@ namespace GetNumberOfK {
 
     class Solution {
 
+        /// <summary>
+        /// 解法，二分查找
+        /// 在排序数组中查找，首先想到的是二分查找
+        /// 由于数组中的数字都是整数，所以可以通过分别查找k+0.5和k-0.5这两个数字应该插入的位置
+        /// 两个位置的差值就是数字k的个数
+        /// 由于题目未说明排序数组是递增还是递减，所以通过increase标志判断
+        /// </summary>
+
+        public int Find(int[] data, double k){
+            int left = 0, right = data.Length - 1;
+            int mid = 0;
+            bool increase = true;
+            if(data[left] > data[right]){
+                increase = false;
+            }
+            while(left <= right){
+                mid = (left + right) / 2;
+                if(data[mid] < k){
+                    if(increase){
+                        left = mid + 1;
+                    }else{
+                        right = mid - 1;
+                    }
+                    
+                }else if(data[mid] > k){
+                    if(increase){
+                        right = mid - 1;
+                    }else{
+                        left = mid + 1;
+                    }
+                }
+            }
+            return increase ? left : -left;
+        }
+
         public int GetNumberOfK(int[] data, int k)
         {
-            return 0;
+            if(data == null || data.Length == 0){
+                return 0;
+            }
+            return Find(data, k + 0.5) - Find(data, k - 0.5);
         }
 
         public void Test() {
             int[] data = new int[]{1, 2, 3, 3, 4};
+            // data = new int[]{3, 3, 3, 3, 3};
+            // data = null;
+            // data = new int[]{};
+            // data = new int[]{4, 3, 3, 2, 1};
             int k = 3;
 
             Console.WriteLine(GetNumberOfK(data, k));
