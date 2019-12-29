07775ba14d518227dca02ba95f9ea4b802e2d8b8
iwiniwin <iwiniwin@foxmail.com>
2019-12-29 15:54:13 +0800

    add the first solution to the FirstAppearingOnce

--- README.md
+++ README.md
@@ -119,7 +119,7 @@
 <a href="https://github.com/iwiniwin/Algorithm/blob/master/%E5%89%91%E6%8C%87offer/IsNumeric.cs">表示数值的字符串</a>
 </td>
 <td width="25%" align="center">
-
+<a href="https://github.com/iwiniwin/Algorithm/blob/master/%E5%89%91%E6%8C%87offer/FirstAppearingOnce.cs">字符流中第一个不重复的字符</a>
 </td>
 </tr>
 
--- 剑指offer/FirstAppearingOnce.cs
+++ 剑指offer/FirstAppearingOnce.cs
@@ -25,24 +25,47 @@ class Solution
 }
 */
 using System;
+using System.Collections.Generic;
 namespace FirstAppearingOnce {
 
     class Solution {
 
+        /// <summary>
+        /// 解法1
+        /// 基本思路：
+        /// 根据ASCII码一共定义了128个字符，因此使用长度为128的数组记录每个字符的出现字数
+        /// 当每个字符出现1次时，入队列。查找第一个不重复字符时，将出现次数不为1的字符，出队。
+        /// 这样队首的元素就是第一个不重复的字符
+        /// </summary>
+
+        int[] array = new int[128];
+        Queue<char> queue = new Queue<char>();
+
         public char FirstAppearingOnce()
         {
-            return '#';
+            while(queue.Count > 0 && array[queue.Peek()] != 1){
+                queue.Dequeue();
+            }
+            return queue.Count == 0 ? '#' : queue.Peek();
         }
 
         public void Insert(char c)
         {
-            // write code here
+            array[c] ++;
+            if(array[c] == 1){
+                queue.Enqueue(c);
+            }
         }
 
         public void Test() {
-
-            Insert('.');
-
+            // Insert('.');
+            Insert('g');
+            Insert('o');
+            Insert('o');
+            Insert('g');
+            Insert('l');
+            Insert('e');
+            
             Console.WriteLine(FirstAppearingOnce());
         }
     }
