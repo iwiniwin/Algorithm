03f2034210956a7b7824c9700a2bc9814dae1f4d
iwiniwin <iwiniwin@foxmail.com>
2020-01-03 21:55:06 +0800

    add the solution to the GetNext

--- 剑指offer/GetNext.cs
+++ 剑指offer/GetNext.cs
@@ -32,15 +32,58 @@ namespace GetNext {
 
     class Solution {
 
+        /// <summary>
+        /// 解法
+        /// 基本思路：
+        /// 中序遍历的顺序是先左节点，再根节点，再右节点
+        /// 1. 节点为空，返回空
+        /// 2. 如果节点有右节点，则一直向下寻找该右节点的左节点，直到叶子节点为止，即为下一个节点
+        /// 3. 如果节点有父节点，且是父节点的左节点，则返回父节点。否则继续判断其父节点是否满足条件，直到父节点为空。
+        /// 前中后续遍历介绍 https://blog.csdn.net/FightLei/article/details/89281600
+        /// </summary>
+
         public TreeLinkNode GetNext(TreeLinkNode pNode)
         {
-            return pNode;
+            if(pNode == null){
+                return null;
+            }
+
+            if(pNode.right != null){
+                pNode = pNode.right;
+                while(pNode.left != null){
+                    pNode = pNode.left;
+                }
+                return pNode;
+            }
+
+            while(pNode.next != null){
+                if(pNode == pNode.next.left){
+                    return pNode.next;
+                }
+                pNode = pNode.next;
+            }
+
+            return null;
         }
 
         public void Test() {
-            TreeLinkNode pNode = new TreeLinkNode(0);
+            TreeLinkNode pNode = null;
+            pNode = new TreeLinkNode(0);
+            pNode.left = new TreeLinkNode(1);
+            pNode.left.next = pNode;
 
+            pNode.left.right = new TreeLinkNode(3);
+            pNode.left.right.next = pNode.left;
+
+
+            pNode.right = new TreeLinkNode(2);
+            pNode.right.next = pNode;
+
+            pNode.right.left = new TreeLinkNode(4);
+            pNode.right.left.next = pNode.right;
+            
             TreeLinkNode node = GetNext(pNode);
+
             if (node == null){
                 Console.WriteLine("null");
             }else{
