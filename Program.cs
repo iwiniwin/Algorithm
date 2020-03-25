6aea77d8a0074535eac229fa56cd5c58a9348392
iwiniwin <iwiniwin@foxmail.com>
2020-03-25 19:27:10 +0800

    add Fibonacci problem

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ namespace Algorithm
             // new PrintListFromTailToHead.Solution().Test();
             // new ReConstructBinaryTree.Solution().Test();
             // new SimulateQueueWithStack.Solution().Test();
-            new MinNumberInRotateArray.Solution().Test();
+            // new MinNumberInRotateArray.Solution().Test();
+            new Fibonacci.Solution().Test();
             // new Permutation.Solution().Test();
             // new MoreThanHalfNum.Solution().Test();
             // new GetLeastNumbers.Solution().Test();
---
+++ 剑指offer/Fibonacci.cs
@@ -0,0 +1,35 @@
+/*
+题目名称：
+斐波那契数列
+
+题目描述：
+大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+n<=39
+
+代码结构：
+class Solution
+{
+    public int Fibonacci(int n)
+    {
+        // write code here
+    }
+}
+*/
+using System;
+namespace Fibonacci {
+
+    class Solution {
+
+        public int Fibonacci(int n)
+        {
+            return 0;
+        }
+
+        public void Test() {
+
+            int n = 1;
+
+            Console.WriteLine(Fibonacci(n));
+        }
+    }
+}
