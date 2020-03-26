2e5ad9a59a867506a27defff5ee7ea8123a5454c
iwiniwin <iwiniwin@foxmail.com>
2020-03-26 14:37:55 +0800

    add the third solution to the Fibonacci

--- 剑指offer/Fibonacci.cs
+++ 剑指offer/Fibonacci.cs
@@ -57,6 +57,53 @@ namespace Fibonacci {
             return i;
         }
 
+        /// <summary>
+        /// 解法3，矩阵的快速幂
+        /// 基本思路：
+        /// 首先归纳出斐波那契数列和矩阵的关系
+        /// 我们已知斐波那契第n项，Fn = F(n - 1) + F(n - 2)，可以将它们转换成如下所示的矩阵形式
+        /// 
+        ///     Fn              Fn-1 + Fn-2            Fn-1 * 1 + Fn-2 * 1        1  1       Fn-1
+        /// [        ]  =  [                    ] = [                      ]  = [      ] * [      ]
+        ///     Fn-1                Fn-1               Fn-1 * 1 + Fn-2 * 0        1  0       Fn-2
+        /// 
+        /// 进而推导出
+        /// 
+        ///     Fn           1  1                   F1
+        /// [        ]  =  [      ]的 n - 1次方 * [     ]
+        ///     Fn-1         1  0                   F0
+        /// 
+        /// 然后直接编程计算矩阵乘法以及矩阵快速幂并代入公式即可
+        /// 快速幂以及详细推导过程介绍 https://www.cnblogs.com/iwiniwin/p/10798884.html
+        /// </summary>
+
+        public int[,] multiply(int[,] m1, int[,] m2){
+            return new int[,]{
+                {m1[0, 0] * m2[0, 0] + m1[0, 1] * m2[1, 0], m1[0, 0] * m2[0, 1] + m1[0, 1] * m2[1, 1]},
+                {m1[1, 0] * m2[0, 0] + m1[1, 1] * m2[1, 0], m1[1, 0] * m2[0, 1] + m1[1, 1] * m2[1, 1]},
+            };
+        }
+
+        public int[,] pow(int[,] matrix, int n){
+            int[,] ret = new int[,]{{1, 0}, {0, 1}};
+            while(n > 0){
+                if((n & 1) > 0) {
+                    ret = multiply(ret, matrix);
+                }
+                n = n >> 1;
+                matrix = multiply(matrix, matrix);
+            }
+            return ret;
+        }
+
+        public int Fibonacci3(int n)
+        {
+            int[,] unit = new int[,]{{1, 1}, {1, 0}};
+            int[,] matrix = new int[,]{{1, 0}, {0, 0}};
+            int[,] ret = multiply(pow(unit, n - 1), matrix);
+            return n == 0 ? 0 : ret[0, 0];
+        }
+
         public void Test() {
 
             int n = 0; // 0
@@ -68,7 +115,8 @@ namespace Fibonacci {
             n = 39;
 
             // Console.WriteLine(Fibonacci(n));
-            Console.WriteLine(Fibonacci2(n));
+            // Console.WriteLine(Fibonacci2(n));
+            Console.WriteLine(Fibonacci3(n));
         }
     }
 }
