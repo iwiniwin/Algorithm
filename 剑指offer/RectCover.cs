3d34ab9a1a3ba9f66702d4220c8e0c20c91753c1
iwiniwin <iwiniwin@foxmail.com>
2020-04-20 11:39:19 +0800

    add RectCover problem

---
+++ 剑指offer/RectCover.cs
@@ -0,0 +1,37 @@
+/*
+题目名称：
+矩形覆盖
+
+题目描述：
+我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+
+比如n=3时，2*3的矩形块有3种覆盖方法
+
+代码结构：
+class Solution
+{
+    public int rectCover(int number)
+    {
+        // write code here
+    }
+}
+*/
+using System;
+namespace RectCover {
+
+    class Solution {
+
+        public int RectCover(int number)
+        {
+            return 0;
+        }
+
+        public void Test() {
+
+            int number = 6;
+
+            Console.WriteLine(RectCover(number));
+        }
+    }
+}
