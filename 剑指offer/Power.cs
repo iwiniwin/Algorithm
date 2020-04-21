6028a41ece1102e8194f4b992e7debf7a84a8479
iwiniwin <iwiniwin@foxmail.com>
2020-04-21 16:43:18 +0800

    add Power problem

---
+++ 剑指offer/Power.cs
@@ -0,0 +1,37 @@
+/*
+题目名称：
+数值的整数次方
+
+题目描述：
+给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+
+保证base和exponent不同时为0
+
+代码结构：
+class Solution
+{
+    public double Power(double thebase, int exponent)
+    {
+        // write code here
+    }
+}
+*/
+using System;
+namespace Power {
+
+    class Solution {
+
+        public double Power(double thebase, int exponent)
+        {
+            return 0;
+        }
+
+        public void Test() {
+
+            double thebase = 3;
+            int exponent = 4;
+
+            Console.WriteLine(Power(thebase, exponent));
+        }
+    }
+}
