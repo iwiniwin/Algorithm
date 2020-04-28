02363a2b5a28f95a2f8d824900696446d1c8489f
iwiniwin <iwiniwin@foxmail.com>
2020-04-28 17:01:25 +0800

    add the second solution to the VerifySquenceOfBST

--- 剑指offer/VerifySquenceOfBST.cs
+++ 剑指offer/VerifySquenceOfBST.cs
@@ -56,14 +56,39 @@ namespace VerifySquenceOfBST {
             return VerifySquenceOfBSTImpl(sequence, left, index) && VerifySquenceOfBSTImpl(sequence, index, right - 1);
         }
 
+        /// <summary>
+        /// 解法2，非递归
+        /// 基本思路：
+        /// 采用非递归形式，但其实仍然是递归思想
+        /// 与解法1将去掉根元素的序列看成是左右两个子树不同的是
+        /// 非递归算法是将剩下的序列看成是一棵子树，即右子树。
+        /// 原来的左子树被看成是右子树的左子树
+        /// 根据二叉搜索树的性质，右子树的所有元素一定是大于左子树的所有元素的，所以这种转换是成立的
+        /// </summary>
+
+        public bool VerifySquenceOfBST2(int[] sequence)
+        {
+            if(sequence == null || sequence.Length == 0) return false;
+            int len = sequence.Length, i = 0;
+            while(--len > 0){
+                while(sequence[i] < sequence[len]) i ++;
+                while(sequence[i] > sequence[len]) i ++;
+                if(i < len) return false;
+                i = 0;
+            }
+            return true;
+        }
+
         public void Test() {
             
             int[] sequence = new int[]{1, 2, 3, 4, 5};
-            sequence = new int[]{1, 6, 5, 9, 13, 12, 8};
-            sequence = new int[]{1, 6, 5, 9, 11, 7, 8};
+            sequence = new int[]{5, 4, 3, 2, 1};
+            // sequence = new int[]{1, 6, 5, 9, 13, 12, 8};
+            // sequence = new int[]{1, 6, 5, 9, 11, 7, 8};
             // sequence = new int[]{};
 
-            Console.WriteLine(VerifySquenceOfBST(sequence));
+            // Console.WriteLine(VerifySquenceOfBST(sequence));
+            Console.WriteLine(VerifySquenceOfBST2(sequence));
         }
     }
 }
