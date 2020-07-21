e39740c310b18718adacb125fbadcbe8cdbbffb6
iwiniwin <iwiniwin@foxmail.com>
2020-07-21 16:57:25 +0800

    add AddStrings problem

---
+++ Leetcode/AddStrings.cs
@@ -0,0 +1,42 @@
+/*
+题目名称：
+字符串相加
+
+题目描述：
+给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+
+注意：
+1. num1 和 num2 的长度都小于 5100.
+2. num1 和 num2 都只包含数字 0-9.
+3. num1 和 num2 都不包含任何前导零。
+4. 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式。
+
+代码结构：
+public class Solution {
+    public string AddStrings(string num1, string num2) {
+
+    }
+}
+
+题目链接：
+https://leetcode-cn.com/problems/add-strings/
+*/
+using System;
+using System.Collections.Generic;
+namespace AddStrings {
+
+    class Solution {
+
+        public string AddStrings(string num1, string num2) {
+            return "579";
+        }
+
+        public void Test() {
+            string num1 = "123";
+
+            string num2 = "456";
+            
+            Console.WriteLine(AddStrings(num1, num2));
+        }
+    }
+}
--- LeetcodeProgram.cs
+++ LeetcodeProgram.cs
@@ -20,7 +20,9 @@ namespace Algorithm
             // new IsValidParentheses.Solution().Test();               // 有效的括号
             // new RemoveDuplicates.Solution().Test();                 // 删除排序数组中的重复项
             // new MaxArea.Solution().Test();                          // 盛最多水的容器
-            new StringMultiply.Solution().Test();                         // 字符串相乘
+            // new StringMultiply.Solution().Test();                   // 字符串相乘（待完成）
+
+            new AddStrings.Solution().Test();                       // 字符串相加
         }
     }
 }
