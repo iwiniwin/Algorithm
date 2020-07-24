af3ead488363203182464c6c96c2b59e385a1ce2
iwiniwin <iwiniwin@foxmail.com>
2020-07-24 17:04:00 +0800

    add the second solution to the ReverseString

--- Leetcode/ReverseString.cs
+++ Leetcode/ReverseString.cs
@@ -31,12 +31,36 @@ namespace ReverseString {
 
     class Solution {
 
+        /// <summary>
+        /// 解法1
+        /// 基本思路：
+        /// 第一个字符与倒数第一个字符交换，第二个与倒数第二个交换，第三个与倒数第三个。。。
+        /// 对于反转字符数组的所有字符，实际上只需要交换 数组长度的一半 次即可
+        /// </summary>
+        
         public void ReverseString(char[] s) {
             int len = s.Length;
             for(int i = 0; i < len / 2; i ++){
                 char temp = s[i];
                 s[i] = s[len - 1 - i];
                 s[len - 1 - i] = temp;
+            }
+        }
+
+        /// <summary>
+        /// 解法2，双指针
+        /// 基本思路：
+        /// 使用left指针指向数组头部，right指针指向数组尾部
+        /// 交换left与right位置的元素同时，left向右移动，right向左移动
+        /// 直到两个指针相遇则所有元素替换完毕
+        /// </summary>
+        
+        public void ReverseString2(char[] s) {
+            int left = 0, right = s.Length - 1;
+            while(left < right){
+                char temp = s[left];
+                s[left++] = s[right];
+                s[right--] = temp;
             }
         }
 
@@ -49,11 +73,12 @@ namespace ReverseString {
 
         public void Test() {
             char[] s = new char[]{'h','e','l','l','o'};
-            s = new char[]{'H', 'a', 'n', 'n', 'a', 'h'};
-            s = new char[]{};
-            s = new char[]{'a'};
+            // s = new char[]{'H', 'a', 'n', 'n', 'a', 'h'};
+            // s = new char[]{};
+            // s = new char[]{'a'};
             
-            ReverseString(s);
+            // ReverseString(s);
+            ReverseString2(s);
 
             Print(s);
         }
